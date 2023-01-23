78fc2e92f35231ac182b0ddcaed0ce9af8031f4a
giovanni <giovanni_ge@hotmai.com>
2023-01-23 14:41:02 -0300

    fix: adicionando alteração no projeto

--- dt-monay/src/App.tsx
+++ dt-monay/src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { defaultTheme } from './styles/themes/default'
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>teste</div>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <h1>Hello World</h1>
+    </ThemeProvider>
   )
 }
 
