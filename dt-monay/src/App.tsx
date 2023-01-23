2bbc8bfaba8dd28b3f1e4eb38aa74444f96745aa
giovanni <giovanni_ge@hotmai.com>
2023-01-23 15:05:27 -0300

    feat: criando componente de header

--- dt-monay/src/App.tsx
+++ dt-monay/src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import { defaultTheme } from './styles/themes/default'
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
+import { Transactions } from './pages/Transactions';
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       
-      <h1>Hello World</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
