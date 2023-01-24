edfcf52c19203c546b8a39560dfd3856aea56e03
giovanni <giovanni_ge@hotmai.com>
2023-01-24 16:20:27 -0300

    fix: adicionando formatação

--- dt-monay/src/pages/Transactions/index.tsx
+++ dt-monay/src/pages/Transactions/index.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { TransactionsContext } from "../../context/TransactionsContext";
+import { dateFormater, priceFormatter } from "../../ultis/formatter";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
@@ -32,9 +33,16 @@ export function Transactions() {
               return (
                 <tr key={transaction.id}>
                 <td width="50%">{transaction.description}</td>
-                <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighlight>
+                </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>
+                  {dateFormater.format(new Date(transaction.createdAt))}
+                </td>
               </tr>
               )
             })}
